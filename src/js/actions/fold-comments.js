fbdc4f140582f30c7973e846f37c56fdea47311c
David Mzareulyan <david@hiero.ru>
2015-05-07 10:11:35 +0300

    Лучший алгоритм фолдинга комментариев

--- src/js/actions/fold-comments.js
+++ src/js/actions/fold-comments.js
@@ -4,31 +4,33 @@ var h = require("../utils/html");
 module.exports = function (node) {
     node = node || document.body;
 
-    forSelect(node, ".p-timeline-post > .comments:not(.be-fe-foldApplied)", function (node) {
-        node.classList.add("be-fe-foldApplied");
+    forSelect(node, ".p-timeline-post > .comments", function (node) {
         var comments = node.querySelectorAll(":scope > .ember-view"),
-            nComments = comments.length;
+            nComments = comments.length,
+            i;
+
+        for (i = 0; i < nComments; i++) {
+            if (nComments > 7 && i > 0 && i < nComments - 1) {
+                comments[i].classList.add("be-fe-comment-folded");
+            } else {
+                comments[i].classList.remove("be-fe-comment-folded");
+            }
+        }
 
         if (nComments <= 7) return;
 
-        var lastComment = comments[nComments - 1];
-
-        for (var i = 1; i < nComments - 1; i++) {
-            node.removeChild(comments[i]);
+        var foldRow = node.querySelector(".be-fe-comments-fold-handle");
+        if (!foldRow) {
+            var foldLink = h("a", (nComments - 2) + " more comments");
+            foldRow = h(".be-fe-comments-fold-handle", foldLink);
+            foldLink.addEventListener("click", function (e) {
+                e.preventDefault();
+                node.classList.add("be-fe-comments-expanded");
+            });
+        } else {
+            foldRow.querySelector("a").innerHTML = (nComments - 2) + " more comments";
         }
 
-        var foldLink = h("a", (nComments - 2) + " more comments");
-        var foldRow = h(".be-fe-comments-fold-handle", foldLink);
-
-        node.insertBefore(foldRow, lastComment);
-
-        foldLink.addEventListener("click", function (e) {
-            e.preventDefault();
-            node.removeChild(foldRow);
-            for (var i = 1; i < nComments - 1; i++) {
-                node.insertBefore(comments[i], lastComment);
-            }
-        });
-
+        node.insertBefore(foldRow, comments[1]);
     });
 };
