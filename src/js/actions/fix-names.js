227b57bf223f1ef01c612316846f9e48fac434bf
David Mzareulyan <david@hiero.ru>
2015-05-09 14:27:51 +0300

    Правка ников на странице списка френдов

--- src/js/actions/fix-names.js
+++ src/js/actions/fix-names.js
@@ -4,10 +4,32 @@ module.exports = function (node) {
     node = node || document.body;
 
     forSelect(node, ".post-body > .title a:not(.be-fe-nameFixed), .p-comment-body .author a:not(.be-fe-nameFixed), .p-timeline-user-like > a:not(.be-fe-nameFixed)", function (node) {
+        node.classList.add("be-fe-nameFixed");
         var login = node.getAttribute("href").substr(1);
         var name = node.innerHTML;
         node.innerHTML = login;
         node.setAttribute("title", name);
-        node.classList.add("be-fe-nameFixed");
     });
+
+    // особый случай: список френдов
+    forSelect(node, ".p-timeline-subscription-user a:not(.be-fe-nameFixed)", function (node) {
+        node.classList.add("be-fe-nameFixed");
+        var login = node.getAttribute("href").substr(1);
+        var name = "";
+        var c = node.firstChild;
+        while (c) {
+            if (c.nodeType == Node.TEXT_NODE) {
+                name = c.nodeValue.replace(/^\s*|\s*$/, "");
+                if (name !== "") {
+                    c.nodeValue = login;
+                    break;
+                }
+            }
+            c = c.nextSibling;
+        }
+        if (name !== "") {
+            node.setAttribute("title", name);
+        }
+    });
+
 };
