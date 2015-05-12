63cac62daac9f2569992c5c7476f010d34872944
David Mzareulyan <david@hiero.ru>
2015-05-12 17:21:02 +0300

    Можно скрывать посты не от френдов

--- src/js/actions/where-this-post-from.js
+++ src/js/actions/where-this-post-from.js
@@ -3,12 +3,33 @@ var forSelect = require("../utils/for-select");
 var h = require("../utils/html");
 
 module.exports = function (node, settings) {
+    // включаемся только на френдленте
+    if (location.pathname !== "/") return;
+
+    if (node === undefined) {
+        setInterval(function () {
+            var hideAliens = !!localStorage["be-fe.hide-alien-posts"];
+            var chk = document.body.querySelector(".be-fe-hide-aliens-switcher input");
+            var posts = document.body.querySelector(".posts");
+            var counter = document.body.querySelector(".be-fe-hide-aliens-counter");
+
+            if (chk && chk.checked !== hideAliens) chk.checked = hideAliens;
+            if (posts && posts.classList.contains("be-fe-hide-aliens") !== hideAliens) {
+                if (hideAliens) {
+                    posts.classList.add("be-fe-hide-aliens");
+                } else {
+                    posts.classList.remove("be-fe-hide-aliens");
+                }
+            }
+            if (counter && posts) {
+                counter.innerHTML = posts.querySelectorAll(".be-fe-post-from-alien").length;
+            }
+        }, 500);
+    }
+
     node = node || document.body;
 
     var useScreenNames = !settings["fix-names"];
-
-    // включаемся только на френдленте
-    if (location.pathname !== "/") return;
 
     forSelect(node, ".timeline-post-container:not(.be-fe-where-from)", function (node) {
         node.classList.add("be-fe-where-from");
@@ -23,6 +44,8 @@ module.exports = function (node, settings) {
         IAm.ready.then(function (iAm) {
             var aType = iAm.whoIs(postAuthor);
             if (aType === IAm.ME || aType === IAm.FRIEND) return;
+
+            node.classList.add("be-fe-post-from-alien");
 
             // пытаемся выяснить, почему мы это видим
             getPostInfo(postId).then(function (postInfo) {
@@ -63,6 +86,25 @@ module.exports = function (node, settings) {
                 }
             });
         });
+
+        if (!document.body.querySelector(".be-fe-hide-aliens-switcher")) {
+            var chk = null;
+            document.body.querySelector(".box-header-timeline").appendChild(
+                h(".be-fe-hide-aliens-switcher",
+                    h("label",
+                        (chk = h("input", {type: "checkbox"})),
+                        " hide non-friends posts (",
+                        h("span.be-fe-hide-aliens-counter", "0"),
+                        ")"
+                    )
+                )
+            );
+            chk.addEventListener("change", function () {
+                localStorage["be-fe.hide-alien-posts"] = chk.checked ? "1" : "";
+            });
+
+            chk.checked = !!localStorage["be-fe.hide-alien-posts"];
+        }
     });
 };
 
