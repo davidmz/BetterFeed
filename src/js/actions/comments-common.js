406e7a5d8e53e3ec82b34ab41ef433ab538ad47f
David Mzareulyan <david@hiero.ru>
2015-05-26 15:50:00 +0300

    Правильная разметка комментариев при раскрытии длинных тредов

--- src/js/actions/comments-common.js
+++ src/js/actions/comments-common.js
@@ -12,44 +12,27 @@ module.exports = function (node) {
         myLogin = loggedLink.getAttribute("href").substr(1);
     }
 
-    forSelect(node, ".post-body", function (node) {
-        var postAuthor = node.querySelector(".title a").getAttribute("href").substr(1);
-        forSelect(node, ".comment:not(.be-fe-comment-from)", function (node) {
-            var authorLink = node.querySelector(".author a");
-            if (!authorLink) return;
-            var author = authorLink.getAttribute("href").substr(1);
-            IAm.ready.then(function (iAm) {
-                var type = iAm.whoIs(author);
-                if (type === IAm.ME) {
-                    node.classList.add("be-fe-comment-from-me");
-                } else if (type === IAm.FRIEND) {
-                    node.classList.add("be-fe-comment-from-friend");
-                } else if (type === IAm.READER) {
-                    node.classList.add("be-fe-comment-from-reader");
-                } else if (type === IAm.STRANGER) {
-                    // pass
-                }
-            });
-            if (author == postAuthor) {
-                node.classList.add("be-fe-comment-from-post-author");
-            }
-            node.classList.add("be-fe-comment-from");
-            node.classList.add("be-fe-comment-from-u-" + author);
-            node.dataset["author"] = author;
-        });
-    });
-
     forSelect(node, ".comment:not(.be-fe-comment-from)", function (node) {
         var postBody = closestParent(node, ".post-body");
         if (!postBody) return;
 
-        var postAuthor = closestParent(node, ".post-body").querySelector(".title a").getAttribute("href").substr(1);
+        var postAuthor = postBody.querySelector(".title a").getAttribute("href").substr(1);
         var authorLink = node.querySelector(".author a");
         if (!authorLink) return;
         var author = authorLink.getAttribute("href").substr(1);
-        if (author == myLogin) {
-            node.classList.add("be-fe-comment-from-me");
-        } else if (author == postAuthor) {
+        IAm.ready.then(function (iAm) {
+            var type = iAm.whoIs(author);
+            if (type === IAm.ME) {
+                node.classList.add("be-fe-comment-from-me");
+            } else if (type === IAm.FRIEND) {
+                node.classList.add("be-fe-comment-from-friend");
+            } else if (type === IAm.READER) {
+                node.classList.add("be-fe-comment-from-reader");
+            } else if (type === IAm.STRANGER) {
+                // pass
+            }
+        });
+        if (author == postAuthor) {
             node.classList.add("be-fe-comment-from-post-author");
         }
         node.classList.add("be-fe-comment-from");
