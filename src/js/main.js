1ab3e45d6c64ce80bd3b95849686851a0c7983c4
David Mzareulyan <david@hiero.ru>
2015-05-29 20:32:51 +0300

    Не запускаем скрипт на страницах файлов и аттачей

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,35 @@
-require('../less/main.less');
+if (!/^\/(attachments|files)\//.test(location.pathname)) {
 
-var settingsStore = require("./settings-store");
+    require('../less/main.less');
 
-var actions = [
-    require("./actions/settings-link"),
-    require("./actions/comments-common")
-];
+    var settingsStore = require("./settings-store");
 
-settingsStore.init();
-settingsStore.loadSettings()
-    .then(function (settings) {
-        for (var k in settings) {
-            if (settings.hasOwnProperty(k) && settings[k] === true) {
-                actions.push(require("./actions/" + k));
-            }
-        }
-        var observer = new MutationObserver(function (mutations) {
-            mutations.forEach(function (mutation) {
-                var i, l, node;
-                for (i = 0, l = mutation.addedNodes.length; i < l; i++) {
-                    node = mutation.addedNodes[i];
-                    if (node.nodeType == Node.ELEMENT_NODE) {
-                        actions.forEach(function (act) { act(node, settings); });
-                    }
+    var actions = [
+        require("./actions/settings-link"),
+        require("./actions/comments-common")
+    ];
+
+    settingsStore.init();
+    settingsStore.loadSettings()
+        .then(function (settings) {
+            for (var k in settings) {
+                if (settings.hasOwnProperty(k) && settings[k] === true) {
+                    actions.push(require("./actions/" + k));
                 }
+            }
+            var observer = new MutationObserver(function (mutations) {
+                mutations.forEach(function (mutation) {
+                    var i, l, node;
+                    for (i = 0, l = mutation.addedNodes.length; i < l; i++) {
+                        node = mutation.addedNodes[i];
+                        if (node.nodeType == Node.ELEMENT_NODE) {
+                            actions.forEach(function (act) { act(node, settings); });
+                        }
+                    }
+                });
             });
-        });
 
-        actions.forEach(function (act) { act(undefined, settings); });
-        observer.observe(document.body, {childList: true, subtree: true});
-    });
+            actions.forEach(function (act) { act(undefined, settings); });
+            observer.observe(document.body, {childList: true, subtree: true});
+        });
+}
