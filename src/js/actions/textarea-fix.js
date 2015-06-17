883df6787224c11e09e65bc1576a032d749dc907
David Mzareulyan <david@hiero.ru>
2015-06-17 12:04:44 +0300

    Убираем борьбу с \n в полях ввода

--- src/js/actions/textarea-fix.js
+++ src/js/actions/textarea-fix.js
@@ -1,34 +1,7 @@
-var closestParent = require("../utils/closest-parent");
 var forSelect = require("../utils/for-select");
-
 
 module.exports = function (node) {
     if (node === undefined) {
-
-        document.addEventListener("keydown", function (e) {
-            if (e.keyCode === 13 && e.target.tagName == "TEXTAREA" && e.target.classList.contains("ember-text-area")) {
-                e.stopPropagation();
-                e.preventDefault();
-            }
-        }, true);
-
-        document.addEventListener("keyup", function (e) {
-            if (e.keyCode === 13 && e.target.tagName == "TEXTAREA" && e.target.classList.contains("ember-text-area")) {
-                e.stopPropagation();
-                e.preventDefault();
-                if (e.target.value !== "") {
-                    var form = closestParent(e.target, ".edit, .create-post");
-                    if (form) {
-                        var btn = form.querySelector("button");
-                        if (btn) {
-                            btn.click();
-                            e.target.style.height = "26px";
-                        }
-                    }
-                }
-            }
-        }, true);
-
         document.addEventListener("input", function (e) {
             if (e.target.tagName == "TEXTAREA" && e.target.classList.contains("ember-text-area")) {
                 if (e.target.scrollHeight > e.target.clientHeight && e.target.scrollHeight < 150) {
