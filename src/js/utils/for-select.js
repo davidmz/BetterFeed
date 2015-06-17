e8643e8c166d596a92761264c4f477bebb985f10
David Mzareulyan <david@hiero.ru>
2015-06-17 17:56:23 +0300

    При перебирании узлов учитываем и корневой узел.

--- src/js/utils/for-select.js
+++ src/js/utils/for-select.js
@@ -7,6 +7,7 @@ module.exports = function (node, selector, foo) {
     if (node === null) return [];
     var nodeList = node.querySelectorAll(selector);
     var nodes = nodeList ? Array.prototype.slice.call(nodeList) : [];
+    if (node.matches(selector)) nodes.unshift(node);
     if (foo) {
         for (var i = 0, l = nodes.length; i < l; i++) {
             foo(nodes[i]);
