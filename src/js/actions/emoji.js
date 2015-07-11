2244f0050e8d983d95a2586cc45a5ebee7fc497c
David Mzareulyan <david@hiero.ru>
2015-07-11 15:48:19 +0300

    Распознаются unicode-emoji, картинки emoji теперь держим в своём репозитории.

--- src/js/actions/emoji.js
+++ src/js/actions/emoji.js
@@ -5,10 +5,16 @@ var bfRoot = require("../utils/bf-root");
 require('../../less/emoji.less');
 
 
-var emoImagesDir = "https://cdn.rawgit.com/Ranks/emojify.js/1.0.5/dist/images/basic",
-    emoImagesAltDir = bfRoot + "/images/emoji",
-    altEmoji = ["neechowsee"],
-    emoTrans = {"ничоси": "neechowsee"};
+var basicEmoRoot = bfRoot + "/images/emoji/basic",
+    customEmoRoot = bfRoot + "/images/emoji/custom",
+    customEmoList = ["neechowsee"],
+    aliases = {
+        "ничоси": "neechowsee",
+        "skeleton": "skull",
+        "poo": "poop",
+        "flame": "fire",
+        "grandma": "older_woman"
+    };
 
 module.exports = function (node) {
     node = node || document.body;
@@ -29,7 +35,7 @@ function emojize(node) {
             c = n;
         }
     }
-    var emoRe = /:([+-]1|ничоси|[a-z0-9_]+):/g;
+    var emoRe = /:([+-]1|e-mail|non-potable_water|ничоси|[a-z0-9_]+):|((?:[\uD800-\uDBFF][\uDC00-\uDFFF])+)|([\u2702-\u27B0\u24C2\u2139\u2300-\u32FF])|(.[\u20E3])/g;
     if (node.nodeType == Node.TEXT_NODE && emoRe.test(node.nodeValue)) {
         emoRe.lastIndex = 0;
         var text = node.nodeValue, match, prevPos = 0, fr = document.createDocumentFragment();
@@ -37,8 +43,32 @@ function emojize(node) {
             if (match.index != prevPos) {
                 fr.appendChild(document.createTextNode(text.substr(prevPos, match.index - prevPos)));
             }
-            var eNode = fr.appendChild(document.createTextNode(match[0]));
-            emoCheck(eNode, match[1]);
+            var i, uni, eNode;
+            if (match[1] !== undefined) { // names
+                eNode = fr.appendChild(document.createTextNode(match[0]));
+                emoCheck(eNode, match[1], match[0], false);
+            } else if (match[2] !== undefined) { // (many) surrogate pairs
+                var txt = match[2], pairs = surPairs(txt), s, code;
+                for (i = 0; i < pairs.length; i++) {
+                    if (isGeo(pairs[i]) && i < pairs.length - 1 && isGeo(pairs[i + 1])) {
+                        s = txt.substr(i * 2, 4);
+                        code = hexInt(pairs[i]) + "-" + hexInt(pairs[i + 1]);
+                        i++;
+                    } else {
+                        s = txt.substr(i * 2, 2);
+                        code = hexInt(pairs[i]);
+                    }
+                    eNode = fr.appendChild(document.createTextNode(s));
+                    emoCheck(eNode, code, s, true);
+                }
+            } else if (match[3] !== undefined) { // dingbats and others 1-symbol
+                eNode = fr.appendChild(document.createTextNode(match[0]));
+                uni = match[3].charCodeAt(0);
+                emoCheck(eNode, hexInt(uni), match[0], true);
+            } else if (match[4] !== undefined) { // 2-symbol
+                eNode = fr.appendChild(document.createTextNode(match[0]));
+                emoCheck(eNode, hexInt(match[4].charCodeAt(0)) + "-" + hexInt(match[4].charCodeAt(1)), match[0], true);
+            }
             prevPos = match.index + match[0].length;
         }
         if (text.length != prevPos) {
@@ -49,16 +79,34 @@ function emojize(node) {
     }
 }
 
-function emoCheck(node, emo) {
-    var code = (emo in emoTrans) ? emoTrans[emo] : emo;
-    var dir = (altEmoji.indexOf(code) !== -1) ? emoImagesAltDir : emoImagesDir;
-    var img = h("img.be-fe-emo", {
-        src: dir + "/" + code + ".png",
-        alt: ":" + emo + ":",
-        title: ":" + emo + ":"
-    });
+function surPairs(text) {
+    var i, pairs = [];
+    for (i = 0; i < text.length; i += 2) {
+        pairs.push((text.charCodeAt(i) - 0xD800) * 0x400 + (text.charCodeAt(i + 1) - 0xDC00) + 0x10000);
+    }
+    return pairs;
+}
+
+function isGeo(uni) { return (uni >= 0x1F1E6 && uni <= 0x1F1FF);}
+
+function emoCheck(node, emo, alt, unicode) {
+    var code = (emo in aliases) ? aliases[emo] : emo;
+    var dir = (customEmoList.indexOf(code) !== -1) ? customEmoRoot : basicEmoRoot;
+    if (unicode) {
+        dir += "/unicode";
+    }
+    var img = h("img.be-fe-emo", {src: dir + "/" + code + ".png", alt: alt});
+    if (!unicode) {
+        img.title = ":" + emo + ":";
+    }
     imgLoaded(img).then(function () {
         node.parentNode.insertBefore(h("span.be-fe-emo-wrap", img), node);
         node.parentNode.removeChild(node);
     });
+}
+
+function hexInt(n) {
+    var s = n.toString(16);
+    while (s.length < 4) s = "0" + s;
+    return s;
 }
