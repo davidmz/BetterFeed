e810df8e7cf5e15180f604c4f0935419846700a3
David Mzareulyan <david@hiero.ru>
2015-07-13 14:39:46 +0300

    Удаляем дубликат автора из адресатов директа + мелкая типографика

--- src/js/actions/direct-style.js
+++ src/js/actions/direct-style.js
@@ -1,4 +1,34 @@
+var forSelect = require("../utils/for-select");
 require('../../less/direct-style.less');
 
-module.exports = function () {};
+module.exports = function (node, settings) {
+    node = node || document.body;
+
+    forSelect(node, ".direct-post .title", function (title) {
+        var addressees = forSelect(title, ".post-addressee");
+        if (addressees.length == 0) {
+            return;
+        }
+        var authorHref = title.querySelector(".post-author").getAttribute("href");
+        var n, c = addressees[0], p = c.parentNode;
+        while (c) {
+            n = c.nextSibling;
+            p.removeChild(c);
+            c = n;
+        }
+
+        addressees
+            .filter(function (a) {
+                return (!a.classList.contains("post-addressee-direct") || a.getAttribute("href") != authorHref);
+            }).forEach(function (a, n, arr) {
+                if (n > 0) {
+                    p.appendChild(document.createTextNode((n === arr.length - 1) ? " and " : ", "));
+                }
+                p.appendChild(a);
+                if (settings["fix-names"] && a.getAttribute("href") === authorHref) {
+                    p.appendChild(document.createTextNode("\u2019" + ((authorHref.substr(-1) !== "s") ? "s" : "") + " feed"));
+                }
+            });
+    });
+};
 
