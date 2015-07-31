d70ea4e09a61d1ddc8b0df90e1c3cd20d9e8776b
David Mzareulyan <david@hiero.ru>
2015-07-31 20:13:16 +0300

    В списках подписок и подписчиков текущего пользователя отмечаются взаимные френды.

--- src/js/actions/show-group-admins.js
+++ src/js/actions/show-group-admins.js
@@ -2,22 +2,59 @@ var forSelect = require("../utils/for-select");
 var api = require("../utils/api");
 var promPlus = require("../utils/promise-tools");
 var h = require("../utils/html");
+var IAm = require("../utils/i-am");
 
 module.exports = function (node) {
     node = node || document.body;
 
-    if (!node.querySelector("h3.p-user-subscribers")) return;
+    var m = /^\/([a-z0-9-]+)\/(subscriptions|subscribers)$/.exec(location.pathname);
+    if (!m) return;
 
-    var username = location.pathname.split("/")[1];
+    var userName = m[1],
+        listName = m[2],
+        n;
 
+    if (listName === "subscribers" && (n = node.querySelector(".p-user-subscribers"))) {
+        IAm.ready.then((iAm) => {
+            if (iAm.whoIs(userName) & IAm.ME) {
+                decorateMutualFriends(iAm, n.nextElementSibling, "p-user-subscriber");
+            } else {
+                decorateGroupAdmins(userName, n.nextElementSibling);
+            }
+        });
+    }
+
+    if (listName === "subscriptions" && (n = node.querySelector(".p-timeline-subscription-users-header"))) {
+        IAm.ready.then((iAm) => {
+            if (iAm.whoIs(userName) & IAm.ME) {
+                decorateMutualFriends(iAm, n.nextElementSibling, "p-timeline-subscription-user");
+            }
+        });
+    }
+
+};
+
+function decorateMutualFriends(iAm, node, chClass) {
+    forSelect(node, `.${chClass} a`, (a) => {
+        var un = a.getAttribute("href").substr(1);
+        if ((iAm.whoIs(un) & (IAm.FRIEND | IAm.READER)) === (IAm.FRIEND | IAm.READER)) {
+            a.classList.add("be-fe-mutual-friend");
+            a.appendChild(h(".be-fe-mutual-friend--crown", {title: "Mutual friend"}));
+        }
+    });
+}
+
+function decorateGroupAdmins(userName, node) {
     promPlus
-        .all([api.get(`/v1/users/${username}`), api.get(`/v1/users/${username}/subscribers`)])
+        .all([api.get(`/v1/users/${userName}`), api.get(`/v1/users/${userName}/subscribers`)])
         .then((resp) => {
             var inf = resp[0].users, subscr = resp[1];
             if (!inf.id || inf.type !== "group") return;
 
+            var admIds = ("administratorIds" in inf) ? inf.administratorIds : inf.administrators;
+
             subscr.subscribers
-                .filter((s) => (inf.administratorIds.indexOf(s.id) !== -1))
+                .filter((s) => (admIds.indexOf(s.id) !== -1))
                 .map((s) => s.username)
                 .forEach((username) => {
                     var a = document.querySelector(`.p-user-subscriber a[href="/${username}"]:not(.be-fe-group-admin)`);
@@ -27,4 +64,4 @@ module.exports = function (node) {
                     }
                 });
         });
-};
+}
