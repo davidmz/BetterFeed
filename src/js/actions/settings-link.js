e98fa83f85246e47849b3cf2c63aaf1beb24c166
David Mzareulyan <david@hiero.ru>
2015-10-11 15:51:14 +0300

    Возможность посмотреть свой accessToken на странице https://freefeed.net/settings

--- src/js/actions/settings-link.js
+++ src/js/actions/settings-link.js
@@ -1,6 +1,8 @@
 import h from "../utils/html.js";
 import forSelect from "../utils/for-select.js";
 import bfRoot from "../utils/bf-root.js";
+import { authToken } from '../utils/current-user-id.js';
+import escapeHTML from "../utils/escape-html.js";
 require('../../less/lightbox.less');
 
 /** @type {Element} */
@@ -39,6 +41,41 @@ function showSettings() {
     lightBox.classList.remove("hidden");
 }
 
+function showAccessToken() {
+    document.body.appendChild(lightBox);
+    lightBoxCont.innerHTML = `
+    <div class="be-fe-popup-page">
+        <p>
+            Your access token is:
+        </p>
+        <p class="be-fe-access-token">
+            ${escapeHTML(authToken)}
+        </p>
+        <p>
+            <strong>WARNING:</strong> Keep your token secure at all times and do not share it with services you do not fully trust.
+            They will be able to perform ANY operation on your behalf including changing your password and
+            <strong>locking you out of your account</strong>.
+        </p>
+        <p>
+            Giving your token to third party services provides them with FULL and UNLIMITED access to your account.
+            You can not change the token, you cannot revert access once you give it out.
+        </p>
+        <p>
+            If you suspect your token is being abused, contact <a href="mailto:freefeed.net@gmail.com">freefeed.net@gmail.com</a> <strong>immediately</strong>.
+        </p>
+        <p style="text-align: center">
+            <button class="be-fe-close-popup-button">Close</button>
+        </p>
+    </div>
+    `;
+    lightBox.classList.remove("hidden");
+    lightBoxCont.querySelector(".be-fe-popup-page").addEventListener('click', e => e.stopPropagation());
+    lightBoxCont.querySelector(".be-fe-close-popup-button").addEventListener('click', () => {
+        lightBoxCont.innerHTML = "";
+        lightBox.classList.add("hidden");
+    });
+}
+
 export default function (node) {
     if (!startVersion) return;
 
@@ -49,11 +86,18 @@ export default function (node) {
         var addOnsBlock = inSettingsHeader.parentNode;
         if (!addOnsBlock.querySelector(".be-fe-settings-in-settings")) {
             var sLink = h("a.be-fe-settings-in-settings", "configure settings");
+            var showTokenLink = h("a.be-fe-settings-in-settings", "show access token");
             addOnsBlock.insertBefore(
-                h("p.bg-success", {style: "padding: 1em"}, `BetterFeed enabled (${startVersion}) | `, sLink),
+                h("p.bg-success.be-fe-green",
+                    `BetterFeed enabled (${startVersion}) | `,
+                    h("i.fa.fa-cog"), " ", sLink,
+                    ' | ',
+                    h("i.fa.fa-exclamation-triangle"), " ", showTokenLink
+                ),
                 forSelect(addOnsBlock, ":scope > p")[1]
             );
             sLink.addEventListener("click", () => showSettings());
+            showTokenLink.addEventListener("click", () => showAccessToken());
         }
     }
 
