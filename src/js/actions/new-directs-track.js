5b5e7098f392f7c6f5aec581a911e662f3cb7ee2
David Mzareulyan <david@hiero.ru>
2015-11-24 16:23:04 +0300

    Уведомления о новых или обновлённых (прокомментированных) директ-сообщениях

--- src/js/actions/new-directs-track.js
+++ src/js/actions/new-directs-track.js
@@ -5,59 +5,93 @@ import { authToken } from '../utils/current-user-id';
 import Cell from "../utils/cell";
 import WS from "../utils/ws";
 
-var settings = null;
-var savedPosts = null;
+const MAX_POSTS_COUNT = 100;
 
-var dirStatus = new Cell({hasNew: false, hasUpdated: false});
+var settings = null,
+    initiateSettings,
+    settingsInitiated = new Promise(resolve => initiateSettings = resolve),
+    viewedPosts = null,
+    actualPosts = new Map(),
+    postsChanged = new Cell();
 
 export default function (node, pSettings) {
     if (!settings) {
         settings = pSettings;
-        savedPosts = list2map(settings.directs || []);
+        if (settings.directs !== null) {
+            initiateSettings();
+        }
+        viewedPosts = list2map(settings.directs || []);
     }
 
     if (!node) {
         init();
 
-        dirStatus.onValue(({hasNew, hasUpdated}) => {
-            let el = document.body.querySelector(".be-fe-directs-notify");
-            if (!el) return;
-            if (hasNew) {
-                el.classList.remove("hide");
-                el.classList.add("-with-new");
-                el.setAttribute("title", "New post(s)");
-            } else if (hasUpdated) {
-                el.classList.remove("hide");
-                el.classList.remove("-with-new");
-                el.setAttribute("title", "New comment(s)");
-            } else {
-                el.classList.add("hide");
-            }
-        });
+        postsChanged
+            .map(() => {
+                let hasNew = false,
+                    hasUpdated = false;
+                actualPosts.forEach((upd, id) => {
+                    if (!viewedPosts.has(id)) {
+                        hasNew = true;
+                    } else if (viewedPosts.get(id) < upd) {
+                        hasUpdated = true;
+                    }
+                });
+                return [hasNew, hasUpdated];
+            })
+            .onValue(([hasNew, hasUpdated]) => {
+                let el = document.body.querySelector(".be-fe-directs-notify");
+                if (!el) return;
+                if (hasNew) {
+                    el.classList.remove("hide");
+                    el.classList.add("-with-new");
+                    el.setAttribute("title", "New post(s)");
+                } else if (hasUpdated) {
+                    el.classList.remove("hide");
+                    el.classList.remove("-with-new");
+                    el.setAttribute("title", "New comment(s)");
+                } else {
+                    el.classList.add("hide");
+                }
+            });
     }
 
     node = node || document.body;
 
     forSelect(node, ".sidebar .p-direct-messages", node => {
         node.appendChild(h("i.fa.fa-envelope.be-fe-directs-notify.hide"));
-        dirStatus.poke();
+        postsChanged.poke();
+    });
+
+    forSelect(node, ".direct-post", async node => {
+        let postId = node.dataset.postId;
+        if (!postId) return;
+        await settingsInitiated;
+        let {posts: {id, updatedAt}} = await api.get('/v1/posts/' + postId + '?maxLikes=0&maxComments=0');
+        viewedPosts.set(id, parseInt(updatedAt));
+        postsChanged.poke();
+        settings.directs = map2list(viewedPosts);
+        settings.save();
     });
 }
 
-function newPost(postId, updatedAt, doInsert = false) {
-    if (!doInsert) {
-        if (!savedPosts.has(postId)) {
-            dirStatus.value = {...dirStatus.value, hasNew: true};
-        } else if (updatedAt > savedPosts.get(postId)) {
-            dirStatus.value = {...dirStatus.value, hasUpdated: true};
+function newPost(postId, updatedAt) {
+    // Ждём 200 мс на случай, если пост будет рисоваться на странице
+    setTimeout(() => {
+        if (!document.getElementById(`post-${postId}`)) {
+            // пост не на странице
+            actualPosts.set(postId, updatedAt);
+            postsChanged.poke();
         }
-    } else {
-        savedPosts.set(postId, updatedAt);
-    }
+    }, 200);
 }
 
 function delPost(postId) {
-    // savedPosts.delete(postId);
+    viewedPosts.delete(postId);
+    actualPosts.delete(postId);
+    postsChanged.poke();
+    settings.directs = map2list(viewedPosts);
+    settings.save();
 }
 
 function init() {
@@ -65,26 +99,25 @@ function init() {
     socket.onConnect = async () => {
         let {posts, "timelines": {id}} = await api.get("/v1/timelines/filter/directs?offset=0");
         socket.send("42" + JSON.stringify(["subscribe", {timeline: [id]}]));
-        let initialLoad = (settings.directs === null);
-        posts.forEach(p => newPost(p.id, parseInt(p.updatedAt), initialLoad));
-        if (initialLoad) {
-            settings.directs = map2list(savedPosts);
-            // settings.save();
+        if (settings.directs === null) {
+            posts.forEach(p => viewedPosts.set(p.id, parseInt(p.updatedAt)));
+            postsChanged.poke();
+            settings.directs = map2list(viewedPosts);
+            settings.save();
+            initiateSettings();
+        } else {
+            posts.forEach(p => newPost(p.id, parseInt(p.updatedAt)));
         }
     };
     socket.onMessage = msg => {
         let [code] = /^\d+/.exec(msg);
         if (code != "42") return;
         let [event, obj] = JSON.parse(msg.substr(code.length));
-        console.log(event, obj);
         if (event == "comment:new") {
-            console.log(obj.comments.postId, obj.comments.createdAt);
             newPost(obj.comments.postId, parseInt(obj.comments.createdAt));
         } else if (event == "post:new") {
-            console.log(obj.posts.id, obj.posts.createdAt);
             newPost(obj.posts.id, parseInt(obj.posts.createdAt));
         } else if (event == "post:destroy") {
-            console.log(obj.meta.postId);
             delPost(obj.meta.postId);
         }
     };
@@ -99,5 +132,9 @@ function list2map(list) {
 function map2list(map) {
     let list = [];
     map.forEach((id, updatedAt) => list.push({id, updatedAt}));
+    if (list.length > MAX_POSTS_COUNT) {
+        list.sort((a, b) => b.updatedAt - a.updatedAt);
+        list = list.slice(0, MAX_POSTS_COUNT);
+    }
     return list;
 }
