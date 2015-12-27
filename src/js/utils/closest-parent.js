74eb4182e006e090d7297b62764c429e3d1a3c66
David Mzareulyan <david@hiero.ru>
2015-12-27 20:48:51 +0300

    Нативный метод closest

--- src/js/utils/closest-parent.js
+++ src/js/utils/closest-parent.js
@@ -1,5 +1,7 @@
 import matches from "./matches.js";
 
+var native = !!Element.prototype.closest;
+
 /**
  *
  * @param {Element} element
@@ -9,6 +11,8 @@ import matches from "./matches.js";
  */
 export default function closestParent(element, selector, withSelf = false) {
     var p = withSelf ? element : element.parentNode;
+    if (native) return p.closest(selector);
+
     if (p && p.nodeType == Node.ELEMENT_NODE) {
         return matches(p, selector) ? p : closestParent(p, selector);
     }
